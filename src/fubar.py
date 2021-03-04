91ea0fb7a998c908ff0ed9a8e340e46bcabb8715
L8733 <E6985@protonmail.com>
2021-03-04 09:13:31 +0000

    update repository ./fubar-python - Everything in python is an object.

--- src/fubar.py
+++ src/fubar.py
@@ -1,31 +1,52 @@
+# repository ./E6985
 def fFullScreen():
 	"""Utilise full Jupyter notebook screen.
+
+Input:
+Process: (IPython.core.display.display; IPython.core.display.HTML)
+Output:
 """
 	from IPython.core.display import display,HTML
 	display(HTML("<style>.container { width:100% !important; }</style>"))
-
+# --- END ---
+# repository ./E6985
 def fDarkReader():
-	"""Unable to determine plot ticks when using https://darkreader.org [1][2].
+	"""Unable to determine plot ticks when using https://darkreader.org.
 
-[1] CptanPanic, "How to handle labels when using dark theme with jupyterlabs?," [github](https://web.archive.org/web/20201027100553/https://github.com/matplotlib/ipympl/issues/25), October 2017.
-[2] Anaconda (use), "matplotlib.style.core," Docstring, June 2020.
+Input:
+Process: (matplotlib.style.core.use)
+Output:
 """
 	from matplotlib.style.core import use
 	use({"default"})
-
+# --- END ---
+# repository ./E6985
 def fMagic(nPar):
 	"""None.
 
 Input: Function/Method etc.
-Process: Using built-in: inspect.getmodule; type; __doc__ 
+Process: (inspect.getmodule; type; __doc__) 
 Output:	Function/Method etc additional information.
 """
 	from inspect import getmodule
 	print(getmodule(nPar))
 	print(type(nPar))
 	return nPar.__doc__
-
+# --- END ---
+# repository ./E6985
 if __name__=="__main__":
 	pass
 else:
+	# repository *.ipynb
 	fFullScreen()
+# --- END ---
+# repository ./fubar-python
+def fIsObject(nParSomething):
+    """Everything in python is an object.
+    
+Input: nParSomething
+Process: (isinstance)
+Output: boolean True or False
+"""
+    return isinstance(nParSomething,object)
+# --- END ---
